b8d1b471f62fe223c91511352e2829a7069f1f2e
Sanjay Kumar <sk1635050@gmail.com>
2025-07-24 07:32:46 +0530

    Initial commit

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
     })
 
     let info = await transporter.sendMail({
-      from: `"StudyNotion By Shivanand Yadav " <${process.env.MAIL_USER}>`, // sender address
+      from: `"StudyNotion By Sanjay Kumar " <${process.env.MAIL_USER}>`, // sender address
       to: `${email}`, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
